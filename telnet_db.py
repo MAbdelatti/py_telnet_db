b2c53a65102b6cfcad977d15c64cd9a892104634
MarwanAbdelatti <37384021+MarwanAbdelatti@users.noreply.github.com>
2019-03-01 11:20:22 -0500

    Added continuous looping and confirmation messages

--- telnet_db.py
+++ telnet_db.py
@@ -1,11 +1,15 @@
 import time
+import os
+
 from telnetlib import Telnet
 import pyodbc
 
-CONN_STRING = r'DRIVER=Driver do Microsoft Access (*.mdb);DBQ=C:\Users\Marwan Abdelatti\Desktop\test_1.accdb.mdb'
+db_file = os.path.realpath(__file__)
+CONN_STRING = r'DRIVER=Driver do Microsoft Access (*.mdb);DBQ=c:\users\marwan abdelatti\desktop\py_telnet_db-master\test_1.mdb'
 
 def fetch_data():
         with Telnet('localhost', 9055, 10) as tn:
+                print('Successfully connected to port.')
                 res = tn.read_until(b'AAAAAAAAAAAAAAAAAAAAAAAA', 5).splitlines()
 
         record = [record[:record.index(b'\t')].decode('utf-8') for record in res[1:]]
@@ -14,6 +18,7 @@ def fetch_data():
 def connect_to_db(CONN_STRING):
         conn = pyodbc.connect(CONN_STRING)
         cursor = conn.cursor()
+        print('Successfully connected to database.')
         return(cursor)
 
 def clear_db(cursor):
@@ -29,7 +34,8 @@ def push_data(record_list, cursor):
         cursor.commit()
 
 if __name__ == "__main__":
-        while True:
-                clear_db(connect_to_db(CONN_STRING))
-                push_data(fetch_data(), connect_to_db(CONN_STRING))
+        cursor = connect_to_db(CONN_STRING)
+        while cursor:
+                clear_db(cursor)
+                push_data(fetch_data(), cursor)
                 time.sleep(10)
